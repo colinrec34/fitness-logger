1f5b0a55bd677647976076190eb80cfc5cc2968e
colinrec34 <colinrecker@gmail.com>
2025-07-19 18:49:25 -0700

    Revert "fixed protectedRoute issue"
    This reverts commit 9cce300e109d210cf7f13c070c465cc43f6a04f8.

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,10 +1,9 @@
 // src/components/ProtectedRoute.tsx
 import { useEffect, useState } from "react";
-import type { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { supabase } from "../api/supabaseClient";
 
-export default function ProtectedRoute({ children }: { children: ReactNode }) {
+export default function ProtectedRoute({ children }: { children: JSX.Element }) {
   const [loading, setLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
