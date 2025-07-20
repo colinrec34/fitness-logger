339e5d4cd7ec1c6f4a87032df8913f06d81c0301
colinrec34 <colinrecker@gmail.com>
2025-07-19 19:40:52 -0700

    clean up

--- frontend/src/api/supabaseClient.ts
+++ frontend/src/api/supabaseClient.ts
@@ -11,7 +11,6 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   },
 });
 
-
 export async function getUser(): Promise<User | null> {
   try {
     const { data, error } = await supabase.auth.getSession();
