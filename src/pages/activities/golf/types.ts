27e57b56c945d79f5d5ad746b5d03d57ff86ab3e
colinrec34 <colinrecker@gmail.com>
2026-04-25 11:16:00 -0500

    Add Golfing activity

---
+++ src/pages/activities/golf/types.ts
@@ -0,0 +1,26 @@
+export interface GenericLogData {
+  location_id: string;
+  duration?: number;
+  notes?: string;
+};
+
+export interface LocationRow {
+    id: string;
+    user_id: string;
+    activity_id: string;
+    name: string;
+    lat: number;
+    lon: number;
+    created_at?: string;
+}
+
+export interface LogRow {
+  id: string;
+  user_id: string;
+  activity_id: string;
+  datetime: string;
+  location_id?: string;
+  data: GenericLogData;
+  created_at?: string;
+  updated_at?: string;
+}
